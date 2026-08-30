3775612586cfa568b968be72395af3e5509db588
DruvaRGowda <druvar200628@gmail.com>
2026-08-30 20:15:31 +0530

    Time: 2 ms (55.35%), Space: 32 MB (5.01%) - LeetHub

---
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.cpp
@@ -0,0 +1,74 @@
+class Solution {
+public:
+    vector<int> sortedSquares(vector<int>& nums) {
+        vector<int> pos;
+        vector<int> neg;
+        int siz = nums.size();
+        for(int i = 0 ; i < siz ; i++){
+            if(nums[i] < 0){
+                neg.push_back(nums[i]);
+            }
+            else{
+                pos.push_back(nums[i]); // fixed ;) -> );
+            }
+        } // <-- added this } you missed, so loop ends here
+
+        if(neg.size() == 0){
+            for(int i = 0 ; i<pos.size(); i++){
+                pos[i] = pos[i] * pos[i];
+            }
+            return pos; // <-- moved outside for loop
+        }
+        if(pos.size() == 0){
+            for(int i = 0 ; i<neg.size(); i++){
+                neg[i] = neg[i] * neg[i];
+            }
+            reverse(neg.begin(),neg.end());
+            return neg;
+        }
+
+        int i = 0;
+        int j = 0, id = 0;
+        int n = neg.size();
+        int m = pos.size();
+        vector<int> res(n+m);
+
+        for(i = 0;i < n;i++){
+            neg[i] = neg[i]*neg[i];
+        }
+        reverse(neg.begin(),neg.end()); // added ;
+
+        for(j = 0;j<m;j++){
+            pos[j] = pos[j]*pos[j]; // fixed pos[i] -> pos[j]
+        }
+
+        // reset i,j for merging because you used them above
+        i = 0;
+        j = 0;
+
+        while(i<n && j<m){
+            if(neg[i] <= pos[j]){
+                res[id] = neg[i];
+                id++;
+                i++;
+            }
+            else{
+                res[id] = pos[j];
+                id++;
+                j++; // added ;
+            }
+        }
+        while(i < n){
+            res[id] = neg[i];
+            id++;
+            i++;
+        }
+        while(j < m){
+             res[id] = pos[j];
+             id++;
+             j++; // added ;
+        }
+
+        return res; // added ;
+    }
+};
