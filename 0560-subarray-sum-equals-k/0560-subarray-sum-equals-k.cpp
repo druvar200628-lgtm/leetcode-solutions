0817df25fa89cde230f58800d76aaa2cc1c6ffb3
DruvaRGowda <druvar200628@gmail.com>
2026-09-20 12:52:42 +0530

    Time: 31 ms (95.22%), Space: 45.4 MB (70.71%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -5,7 +5,7 @@ public:
         int count = 0;
         int sum = 0;
         unordered_map<int,int> mp;
-        mp[0] = 1; // sum 0 seen 1 time
+        mp[0] = 1;
 
         for(int i = 0; i < n; i++){
             sum += nums[i];
